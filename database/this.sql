f2f77d2466498cbc0bed519e8b10ed389651dec3
Yrux <yruxwork@gmail.com>
2021-07-08 22:33:09 +0500

    Update this.sql

--- database/this.sql
+++ database/this.sql
@@ -1,9 +1,8 @@
 /*
-SQLyog Community v12.3.3 (32 bit)
-MySQL - 10.1.30-MariaDB : Database - adminiy
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.19-MariaDB : Database - turningheadslashes
 *********************************************************************
 */
-
 
 /*!40101 SET NAMES utf8 */;
 
@@ -13,6 +12,10 @@ MySQL - 10.1.30-MariaDB : Database - adminiy
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`turningheadslashes` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `turningheadslashes`;
+
 /*Table structure for table `adminiy` */
 
 DROP TABLE IF EXISTS `adminiy`;
@@ -20,21 +23,47 @@ DROP TABLE IF EXISTS `adminiy`;
 CREATE TABLE `adminiy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
-  `password` text,
+  `password` text DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `remember_token` text,
+  `remember_token` text DEFAULT NULL,
   `is_active` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `is_deleted` int(11) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `is_deleted` int(11) DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `adminiy` */
 
 insert  into `adminiy`(`id`,`email`,`password`,`name`,`remember_token`,`is_active`,`created_at`,`updated_at`,`is_deleted`) values 
-
 (1,'admin@project.com','$2y$10$fJwkT72wGNXCIBSqq.5JveP/rSFoSRfrSvotM2BJYPO6xgJFgSWVm','Admin','o2JZSUfWev6IQiF8r1uhtvvYriBDmXpXl4Kv3Fc77NJ5BWHks5vwjc0G3Knj',1,'2019-03-28 16:43:17','2019-04-20 15:25:01',0);
+
+/*Table structure for table `faqs` */
+
+DROP TABLE IF EXISTS `faqs`;
+
+CREATE TABLE `faqs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `answer` text DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT 1,
+  `is_deleted` tinyint(4) DEFAULT 0,
+  `user_id` tinyint(4) DEFAULT 0,
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `faqs` */
+
+insert  into `faqs`(`id`,`category_id`,`question`,`answer`,`is_active`,`is_deleted`,`user_id`,`updated_at`,`created_at`) values 
+(1,1963,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown.</p>',1,0,0,'2021-07-08 16:17:15','2021-07-08 16:17:15'),
+(2,1963,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing</p>',1,0,0,'2021-07-08 16:17:30','2021-07-08 16:17:30'),
+(3,1963,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing</p>',1,0,0,'2021-07-08 16:17:41','2021-07-08 16:17:41'),
+(4,1964,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing</p>',1,0,0,'2021-07-08 16:17:50','2021-07-08 16:17:50'),
+(5,1964,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing</p>',1,0,0,'2021-07-08 16:17:57','2021-07-08 16:17:57'),
+(6,1964,'Lorem Ipsum is simply dummy text of the printing','<p>Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing&nbsp;Lorem Ipsum is simply dummy text of the printing</p>',1,0,0,'2021-07-08 16:18:03','2021-07-08 16:18:03');
 
 /*Table structure for table `flag_data` */
 
@@ -44,15 +73,32 @@ CREATE TABLE `flag_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `flag_color` varchar(255) DEFAULT NULL,
   `flag_name` varchar(255) DEFAULT NULL,
-  `flag_reminder` text,
+  `flag_reminder` text DEFAULT NULL,
   `table_name` varchar(100) DEFAULT NULL,
   `ref_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `flag_data` */
+
+/*Table structure for table `images` */
+
+DROP TABLE IF EXISTS `images`;
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(555) DEFAULT NULL,
+  `imageable_id` int(11) DEFAULT NULL,
+  `imageable_type` varchar(255) DEFAULT NULL,
+  `table_name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `images` */
 
 /*Table structure for table `imagetable` */
 
@@ -61,14 +107,14 @@ DROP TABLE IF EXISTS `imagetable`;
 CREATE TABLE `imagetable` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `table_name` varchar(50) NOT NULL,
-  `img_path` text,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `img_path` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `ref_id` int(11) DEFAULT NULL,
-  `type` int(11) DEFAULT '1',
+  `type` int(11) DEFAULT 1,
   `is_active_img` varchar(1) DEFAULT '1',
-  `additional_attributes` text,
-  `img_href` text,
+  `additional_attributes` text DEFAULT NULL,
+  `img_href` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `table_name` (`table_name`,`ref_id`,`type`),
   FULLTEXT KEY `imgp` (`img_path`)
@@ -77,9 +123,7 @@ CREATE TABLE `imagetable` (
 /*Data for the table `imagetable` */
 
 insert  into `imagetable`(`id`,`table_name`,`img_path`,`created_at`,`updated_at`,`ref_id`,`type`,`is_active_img`,`additional_attributes`,`img_href`) values 
-
-(1,'logo','Uploads/imagetable/7afbe7dd3c34cc6b9ec8ac3a6e94ae96/sCtTHox8PjNT2hA2v6agIy34lvyyLsiijWzPgP0A.png','2019-07-23 17:51:33','2019-07-23 12:51:33',0,1,'1',NULL,NULL),
-
+(1,'logo','Uploads/imagetable/1ef7d4325224d71d445a9c17ccec8bf2/1625700074-logo.png','2021-07-08 04:21:14','2021-07-07 23:21:14',0,1,'1',NULL,NULL),
 (2,'favicon','Uploads/imagetable/13c0c92892833baaf750729782ab6104/WZvCOV9PH81W151eRfGCcexCG8T2oBOwF7dv2u53.png','2019-04-18 21:49:42','2019-04-18 16:49:42',0,1,'1',NULL,NULL);
 
 /*Table structure for table `inquiry` */
@@ -90,20 +134,22 @@ CREATE TABLE `inquiry` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `inquiries_name` varchar(255) NOT NULL,
   `inquiries_email` text NOT NULL,
-  `inquiries_phone` varchar(255) NOT NULL,
   `extra_content` text NOT NULL,
   `is_read` char(1) NOT NULL DEFAULT '0',
-  `type` int(11) NOT NULL DEFAULT '1',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `is_active` int(11) NOT NULL DEFAULT '1',
-  `is_deleted` int(11) DEFAULT '0',
-  `user_id` int(11) DEFAULT '0',
+  `type` int(11) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `is_active` int(11) NOT NULL DEFAULT 1,
+  `is_deleted` int(11) DEFAULT 0,
+  `user_id` int(11) DEFAULT 0,
   `inquiries_lname` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `inquiry` */
+
+insert  into `inquiry`(`id`,`inquiries_name`,`inquiries_email`,`extra_content`,`is_read`,`type`,`created_at`,`updated_at`,`is_active`,`is_deleted`,`user_id`,`inquiries_lname`) values 
+(7,'test','test@gmail.com','asdasda','0',1,'2021-07-08 14:53:36','2021-07-08 14:53:36',1,0,0,'test');
 
 /*Table structure for table `m_flag` */
 
@@ -113,40 +159,56 @@ CREATE TABLE `m_flag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `flag_type` varchar(100) NOT NULL,
   `flag_value` varchar(250) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `flag_additionalText` text,
+  `flag_additionalText` text DEFAULT NULL,
   `has_image` varchar(1) DEFAULT '0',
   `is_active` varchar(1) DEFAULT '1',
   `is_config` varchar(1) DEFAULT '0',
-  `flag_show_text` text,
-  `is_featured` int(11) DEFAULT '0',
-  `is_deleted` int(11) DEFAULT '0',
-  `user_id` int(11) DEFAULT '0',
+  `flag_show_text` text DEFAULT NULL,
+  `is_featured` int(11) DEFAULT 0,
+  `is_deleted` int(11) DEFAULT 0,
+  `user_id` int(11) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1963 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1965 DEFAULT CHARSET=utf8;
 
 /*Data for the table `m_flag` */
 
 insert  into `m_flag`(`id`,`flag_type`,`flag_value`,`created_at`,`updated_at`,`flag_additionalText`,`has_image`,`is_active`,`is_config`,`flag_show_text`,`is_featured`,`is_deleted`,`user_id`) values 
-
 (59,'COMPANYPHONE','000-000-0000','2019-04-19 11:33:45','0000-00-00 00:00:00','000-000-0000','0','1','1','Company Phone',0,0,0),
-
-(123,'COMPANY','DUMMY PROJECT','2019-04-29 23:54:46','0000-00-00 00:00:00','Pizza Hut','0','1','1','Company',0,0,0),
-
+(123,'COMPANY','Turning Head Lashes','2021-07-08 22:28:30','0000-00-00 00:00:00','Turning Head Lashes','0','1','1','Company',0,0,0),
 (218,'COMPANYEMAIL','info@demo.com','2019-04-19 11:33:45','0000-00-00 00:00:00','info@demo.com','0','1','1','Company Email',0,0,0),
-
 (499,'CURRENTHEME','red','2019-04-23 03:16:09','0000-00-00 00:00:00','red','0','1','1','Theme Option',0,0,0),
-
 (519,'ADDRESS','Dummy address','2019-04-29 23:54:53','0000-00-00 00:00:00','107 Silver Circle','0','1','1','Address',0,0,0),
-
-(682,'FACEBOOK','https://facebook.com/sdd','2019-04-23 23:56:03','0000-00-00 00:00:00','https://facebook.com/sdd','0','1','1','Facebook',0,0,0),
-
+(682,'FACEBOOK','https://facebook.com','2021-07-08 22:31:54','0000-00-00 00:00:00','https://facebook.com','0','1','1','Facebook',0,0,0),
 (1960,'TWITTER','https://twitter.com/','2019-04-19 11:34:21','0000-00-00 00:00:00','https://twitter.com/','0','1','1','Twitter',0,0,0),
-
 (1961,'INSTAGRAM','https://instagram.com/','2019-04-19 11:34:53','0000-00-00 00:00:00','https://instagram.com/','0','1','1','Instagram',0,0,0),
+(1962,'YOUTUBE','https://youtube.com/','2021-07-08 21:26:53','0000-00-00 00:00:00','https://youtube.com/','0','1','1','Youtube',0,0,0),
+(1963,'FAQCATEGORY','Heading Here','2021-07-08 21:09:39','2021-07-08 16:09:39',NULL,'0','1','0',NULL,0,0,0),
+(1964,'FAQCATEGORY','Heading Here','2021-07-08 16:09:45','2021-07-08 16:09:45',NULL,'0','1','0',NULL,0,0,0);
 
-(1962,'GOOGLEPLUS','https://googleplus.com/','2019-04-19 11:35:03','0000-00-00 00:00:00','https://googleplus.com/','0','1','1','Google+',0,0,0);
+/*Table structure for table `newsletters` */
+
+DROP TABLE IF EXISTS `newsletters`;
+
+CREATE TABLE `newsletters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_active` tinyint(4) DEFAULT 1,
+  `user_id` tinyint(4) DEFAULT 0,
+  `is_deleted` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `newsletters` */
+
+insert  into `newsletters`(`id`,`email`,`updated_at`,`created_at`,`is_active`,`user_id`,`is_deleted`) values 
+(1,'test@gmail.com','2021-07-08 15:50:41','2021-07-08 15:50:41',1,0,0),
+(2,'test1@gmail.com','2021-07-08 15:50:49','2021-07-08 15:50:49',1,0,0),
+(3,'yruxwork@gmail.com','2021-07-08 16:04:58','2021-07-08 16:04:58',1,0,0),
+(4,'pdcustomweb@test.com','2021-07-08 16:05:29','2021-07-08 16:05:29',1,0,0);
 
 /*Table structure for table `password_resets` */
 
@@ -155,8 +217,8 @@ DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE `password_resets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(500) DEFAULT NULL,
-  `token` text,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `token` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -170,12 +232,12 @@ CREATE TABLE `table_notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `table_name` varchar(250) DEFAULT NULL,
   `ref_id` int(11) DEFAULT NULL,
-  `note_value` text,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `note_value` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_id` int(11) DEFAULT NULL,
-  `is_deleted` int(11) DEFAULT '0',
-  `is_active` int(11) DEFAULT '0',
+  `is_deleted` int(11) DEFAULT 0,
+  `is_active` int(11) DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -188,12 +250,12 @@ DROP TABLE IF EXISTS `tracking_users`;
 CREATE TABLE `tracking_users` (
   `request_uri` varchar(500) DEFAULT NULL,
   `request_data_exist` char(1) DEFAULT NULL,
-  `request_data` text,
-  `user_id` int(11) DEFAULT '0',
+  `request_data` text DEFAULT NULL,
+  `user_id` int(11) DEFAULT 0,
   `user_logged_in` char(1) DEFAULT NULL,
-  `session_id` text,
+  `session_id` text DEFAULT NULL,
   `request_lasturi` varchar(500) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `id` int(11) NOT NULL,
   `time_to` timestamp NULL DEFAULT NULL,
@@ -216,34 +278,22 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `password` text NOT NULL,
-  `remember_token` text,
-  `is_active` int(11) DEFAULT '0',
+  `remember_token` text DEFAULT NULL,
+  `is_active` int(11) DEFAULT 0,
   `email_verified_at` datetime DEFAULT NULL,
   `is_deleted` int(11) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `users` */
 
-/*Table structure for table `images` */
-
-DROP TABLE IF EXISTS `images`;
-
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(555) DEFAULT NULL,
-  `imageable_id` int(11) DEFAULT NULL,
-  `imageable_type` varchar(255) DEFAULT NULL,
-  `table_name` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `images` */
+insert  into `users`(`id`,`email`,`name`,`created_at`,`updated_at`,`password`,`remember_token`,`is_active`,`email_verified_at`,`is_deleted`,`lname`) values 
+(3,'test@gmail.com','yrux','2021-07-08 17:00:51','2021-07-08 22:02:07','$2y$10$iBlwhrejXKcTFLXycYYy9uHRpVLQ1QFf2DIvKDTkwnR0J435J1k8y','pg3HD2IQVCFkNLrfVpujJFPR1vjzSkWU00RtxNMFzm3jkL8Owf0V3SuWZPzq',1,NULL,0,'work'),
+(4,'test1@gmail.com','yrux','2021-07-08 17:02:26','2021-07-08 22:02:32','$2y$10$Jb6klWY2bZnjMKYlmHLGBuOlnc14IYg23Z.Q9Kh4Hgb4220UIP5US','AkyHtzvWNqd8t1fsPwGXnu3WKEPW9LaXO9o2NjoU4aEwJ3e9SAfYGYNnrO9H',1,NULL,0,'work2');
 
 /*Table structure for table `ytables` */
 
@@ -251,33 +301,30 @@ DROP TABLE IF EXISTS `ytables`;
 
 CREATE TABLE `ytables` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `js_file` text,
+  `js_file` text DEFAULT NULL,
   `page_heading` varchar(500) DEFAULT NULL,
-  `page_message` text,
+  `page_message` text DEFAULT NULL,
   `new_url` varchar(500) DEFAULT NULL COMMENT 'write urls after base',
   `is_deleted` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `page_limit` enum('10','25','50','100') DEFAULT '10',
-  `fast_crud` int(11) DEFAULT '1',
+  `fast_crud` int(11) DEFAULT 1,
   `model_name` varchar(255) DEFAULT NULL COMMENT 'laravel model name, Fill this when you have different model name and different table name',
   `table_name` varchar(255) DEFAULT NULL COMMENT 'database table name',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 /*Data for the table `ytables` */
 
 insert  into `ytables`(`id`,`js_file`,`page_heading`,`page_message`,`new_url`,`is_deleted`,`created_at`,`updated_at`,`page_limit`,`fast_crud`,`model_name`,`table_name`) values 
-
 (6,'admin/listings/adminiy-listing.js','Adminiy Listing','','adminiy/crud/adminiy',0,'2019-04-03 17:21:50','2019-04-05 16:35:04','',1,'Adminiy','adminiy'),
-
 (7,'admin/listings/table_notes-listing.js','Table Notes','','adminiy.tablename.form',0,'2019-04-05 12:17:58','2019-04-07 02:05:38','',1,'table_notes','table_notes'),
-
 (15,'admin/listings/imagetable-listing.js','Imagetable','','adminiy/crud/imagetable',0,'2019-04-17 19:28:31','2019-04-17 19:28:31','',1,'','imagetable'),
-
 (16,'admin/listings/inquiry-listing.js','Inquiry','','adminiy/crud/inquiry',0,'2019-04-18 17:49:24','2019-04-19 19:42:01','',1,'','inquiry'),
-
-(17,'admin/listings/m_flag-listing.js','Flag','','adminiy/crud/m_flag',0,'2019-04-19 06:51:33','2019-04-19 11:58:07','',1,'','m_flag');
+(17,'admin/listings/m_flag-listing.js','Flag','','adminiy/crud/m_flag',0,'2019-04-19 06:51:33','2019-04-19 11:58:07','',1,'','m_flag'),
+(18,'admin/listings/newsletters-listing.js','Newsletters','','adminiy/crud/newsletters',0,'2021-07-08 15:49:01','2021-07-08 15:49:01','',1,'','newsletters'),
+(19,'admin/listings/faqs-listing.js','Faqs','','adminiy/crud/faqs',0,'2021-07-08 16:13:27','2021-07-08 16:13:27','',1,'','faqs');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
